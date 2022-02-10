6095e74bd9d55643f5a05e60477097d4bb26f707
michalzalobny <michal.zalobny@lepolishbureau.com>
2022-02-11 00:25:43 +0100

    Add for loop example

---
+++ for-loops/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int luckyNumbers[] = {42,123,41,3,6};
+    
+  for(int i = 0; i<=4; i++){
+    printf("%d\n", luckyNumbers[i]);
+  }
+  return 0;
+}
