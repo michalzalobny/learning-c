14f6e06fe4d2933ca44c801360a9bdc4240a4d98
Michal Zalobny <michal.zalobny@lepolishbureau.com>
2022-02-10 16:56:31 +0000

    Add data types chapter

---
+++ data-types/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int age = 40;
+  double gpa = 3.7;
+  char grade = 'A'; //Char always in single quotation marks
+  char phrase[] = "The academy"; //for strings we need the dobule quotations marks
+  return 0;
+}
